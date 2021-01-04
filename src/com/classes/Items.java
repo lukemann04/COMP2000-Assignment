e097edcdbca68a6ef5e1c09f5a5e7b4d8649b85c
lukemann04 <34482771+lukemann04@users.noreply.github.com>
2021-01-04 14:12:08 +0000

    Items class and login system
    Created a class for stock items and implemented a login system for admins.

---
+++ src/com/classes/Items.java
@@ -0,0 +1,26 @@
+package com.classes;
+
+public class Items {
+    private int itemCode;
+    private String itemName;
+    private double itemPrice;
+    private int itemStock;
+
+    public Items(int theItemCode, String theItemName, double theItemPrice, int theItemStock)
+    {
+        itemCode = theItemCode;
+        itemName = theItemName;
+        itemPrice = theItemPrice;
+        itemStock = theItemStock;
+    }
+
+    public int getItemCode() { return itemCode; }
+    public String getItemName() { return itemName; }
+    public double getItemPrice() { return itemPrice; }
+    public int getItemStock() { return itemStock; }
+
+    public void setItemCode(int inItemCode) { this.itemCode = inItemCode; }
+    public void setItemName(String inItemName) { this.itemName = inItemName; }
+    public void setItemPrice(double inItemPrice) { this.itemPrice = inItemPrice; }
+    public void setItemStock(int inItemStock) { this.itemStock = inItemStock; }
+}
---
+++ src/com/gui/AdminLogin.java
@@ -0,0 +1,61 @@
+package com.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AdminLogin extends JFrame{
+    private JPanel mainPanel;
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JLabel lblUsername;
+    private JLabel lblPassword;
+    private JButton btnSubmit;
+    private JLabel txtErrorText;
+
+    public AdminLogin() {
+        setContentPane(mainPanel);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setPreferredSize(new Dimension(300,300));
+        pack();
+
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Check Details
+                try {
+                    File myObj = new File("C:\\Users\\User\\Documents\\GitHub\\COMP2000-Assignment\\src\\com\\admins.txt");
+                    Scanner reader = new Scanner(myObj);
+                    String data;
+                    String pwd = new String(txtPassword.getPassword());
+                    Boolean userFound = false;
+                    while (reader.hasNextLine()) {
+                        data = reader.nextLine();
+                        if(data.equals(txtUsername.getText())){
+                            userFound = true;
+                            data = reader.nextLine();
+                            if(data.equals(pwd)){
+                                //Load Staff Area
+                                StaffArea page = new StaffArea();
+                                page.setVisible(true);
+                            } else{txtErrorText.setText("Incorrect Password");}
+                        }
+                    }
+                    if(userFound = false) {
+                        txtErrorText.setText("User not found.");
+                    }
+                    reader.close();
+                } catch (FileNotFoundException n) {
+                    System.out.println("An error occurred.");
+                    n.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
